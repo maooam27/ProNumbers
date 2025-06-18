e33c8fe676639a08a37e3071168f342d3695bb32
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-18 21:53:02 +0200

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+
+# Main colors, background and foreground
+bgc = "#232A2F"
+fgc = "#bfbfbf"
+
+# Make root window
+root = Tk()
+root.title("ProNumbers - Converting values <3")
+root.geometry("800x600")
+root.resizable(False, False)
+root.configure(bg=bgc)
+
+
+
+if __name__ == "__main__":
+    root.mainloop()
