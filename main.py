5b2aa3d3cf13d485f293a91c9edc5378d0ad73ee
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-18 22:05:01 +0200

    main structure organization

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from tkinter import *
 
 # Main colors, background and foreground
 bgc = "#232A2F"
+bgc_light = "#2B3338"
 fgc = "#bfbfbf"
+fgc_light = "#d0d0d0"
 
 # Make root window
 root = Tk()
@@ -12,7 +14,15 @@ root.geometry("800x600")
 root.resizable(False, False)
 root.configure(bg=bgc)
 
+# Asking for user input
+title = Label(text="ProNumbers", font=("Ubuntu", 26, "bold"), bg=bgc, fg=fgc_light)
+title.pack(padx=10, pady=3, anchor="nw")
 
+subtitle = Label(text="Choose what kind of conversion you wish to do", font=("Ubuntu", 20, "italic"), bg=bgc, fg=fgc)
+subtitle.pack(padx=10, pady=5, anchor="nw")
+
+
+# TODO: choosing list here
 
 if __name__ == "__main__":
     root.mainloop()
