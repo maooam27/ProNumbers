a13799afe1db9e4d8f3efe9e0a12db17fef037d6
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-18 22:24:18 +0200

    testing and learning ttk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 
 # Main colors, background and foreground
@@ -15,14 +16,22 @@ root.resizable(False, False)
 root.configure(bg=bgc)
 
 # Asking for user input
-title = Label(text="ProNumbers", font=("Ubuntu", 26, "bold"), bg=bgc, fg=fgc_light)
+title = ttk.Label(root, text="ProNumbers", font=("Ubuntu", 26, "bold"), foreground=fgc_light, background=bgc)
 title.pack(padx=10, pady=3, anchor="nw")
 
-subtitle = Label(text="Choose what kind of conversion you wish to do", font=("Ubuntu", 20, "italic"), bg=bgc, fg=fgc)
+subtitle = ttk.Label(root, text="Choose what kind of conversion you wish to do", font=("Ubuntu", 20, "italic"), background=bgc, foreground=fgc)
 subtitle.pack(padx=10, pady=5, anchor="nw")
 
 
 # TODO: choosing list here
+country = ttk.Combobox(root)
+country['values'] = ("usa", "italy", "europe", "yay")
+country.pack()
+
+# country.state(["readonly"]) to make it read-only
+
+get_data = ttk.Button(text="get data", command=lambda: print(country.get()))
+get_data.pack()
 
 if __name__ == "__main__":
     root.mainloop()
